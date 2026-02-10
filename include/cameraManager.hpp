d914bac6f56d9130c8a938d15fc2e23ae4047e6b
LucasDAVID <lucas@lucas-david-ry.com>
2026-02-10 16:02:14 +0100

    trying to open camera from opencv

---
+++ include/cameraManager.hpp
@@ -0,0 +1,7 @@
+#ifndef __CAMERA_MANAGER__
+#define __CAMERA_MANAGER__
+
+// Camera manager is the center piece for loading camera contents
+// Take pictures, videos, get camera intrinsic, from here
+
+#endif
