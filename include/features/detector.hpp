f647092e7a7bd78d9ca159129b41415f04859a5f
Cyclemnt <clementlamouller@gmail.com>
2026-02-11 14:11:31 +0100

    First implementation of Harris corner detector from OpenCV

--- include/features/detector.hpp
+++ include/features/detector.hpp
@@ -1,6 +1,17 @@
 #ifndef __DETECTOR__
 #define __DETECTOR__
 
-// Detector class header
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+class Detector {
+public:
+    Detector();
+
+    /// @brief Harris Corner Detection
+    /// @param gray grayscale image
+    /// @return A vector of detected keypoints
+    std::vector<cv::KeyPoint> detect(cv::Mat& gray) const;
+};
 
 #endif
--- src/features/detector.cpp
+++ src/features/detector.cpp
@@ -1,1 +1,31 @@
 #include "features/detector.hpp"
+
+Detector::Detector() {}
+
+std::vector<cv::KeyPoint> Detector::detect(cv::Mat& gray) const {
+    int blockSize = 2;     // blockSize
+    int apertureSize = 3;  // ksize (Sobel)
+    double k = 0.04;       // k (Harris parameter)
+
+    // Harris Corner Detection
+    cv::Mat harrisResponse = cv::Mat::zeros(gray.size(), CV_32FC1);
+    cv::cornerHarris(gray, harrisResponse, blockSize, apertureSize, k);
+
+    std::vector<cv::KeyPoint> keypoints;
+
+    // Define a treshold
+    double maxVal;
+    cv::minMaxLoc(harrisResponse, nullptr, &maxVal);
+    double threshold = 0.01 * maxVal; // relative to the max value
+
+    // Convert harrisResponse to keypoints
+    for (int y = 0; y < harrisResponse.rows; ++y) {
+        for (int x = 0; x < harrisResponse.cols; ++x) {
+            if (harrisResponse.at<float>(y, x) > threshold) {
+                keypoints.emplace_back(cv::Point2f(x, y), 3.0f);
+            }
+        }
+    }
+
+    return keypoints;
+}
