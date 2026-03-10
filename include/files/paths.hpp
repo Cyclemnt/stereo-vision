d3a62330beccf502771dd4c64213cf696f39f728
LucasDAVID <lucas@lucas-david-ry.com>
2026-03-10 18:06:56 +0100

    Modified default paths

--- include/files/paths.hpp
+++ include/files/paths.hpp
@@ -4,12 +4,15 @@
 #include <string>
 #include <filesystem>
 
-namespace paths {
+namespace paths
+{
     // Uses CMake defines variables
-    inline const std::filesystem::path userConfigPath = std::filesystem::path(PATH_CONFIG_FILES); // Where the cameras will be saved/read
-    inline const std::filesystem::path userCameraPath = userConfigPath / "cameras"; // Where the cameras will be saved/read
+    //! Should create the directories at start
+    inline const std::filesystem::path userConfigPath = std::filesystem::path(PATH_CONFIG_FILES); // Where the configurations are stored
+    inline const std::filesystem::path userDevicesPath = userConfigPath / "devices";              // Where the cameras and image's camera settings will be saved/read
+
     inline const std::filesystem::path userDataPath = std::filesystem::path(PATH_DATA_FILES); // Where the results can be output
-    inline const std::filesystem::path userPointCloudPath = userDataPath / "point_clouds"; // Where the results can be output
+    inline const std::filesystem::path userPointCloudPath = userDataPath / "point_clouds";    // Where the point clouds will be exported
 }
 
 #endif
